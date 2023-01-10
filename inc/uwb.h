57aa1cb88f40e3e2bf55fec1fd79070c5bc14d1f
Huo Haodong <huohaodong.io@gmail.com>
2023-01-10 11:03:19 +0000

    rx test

--- inc/uwb.h
+++ inc/uwb.h
@@ -2,24 +2,45 @@
 #define __UWB_H__
 
 #include "libdw3000.h"
-#include "dw3000.h"
-#include "ranging_struct.h"
+#include "dwTypes.h"
+#include "mac_802_15_4.h"
 
-#define UWB_RX_TIMEOUT 0xFFFFF       // RX_TIMEOUT in us
+/* Function Switch */
+#define ENABLE_PHR_EXT_MODE
+
+#define SPEED_OF_LIGHT 299702547
 #define MAX_TIMESTAMP 1099511627776  // 2**40
 #define TX_ANT_DLY 16385
 #define RX_ANT_DLY 16385
 
-#define SPEED_OF_LIGHT 299702547
-#define FRAME_LEN_MAX 127
-#define FRAME_LEN_MAX_EX 1023
+#define FRAME_LEN_STD 127
+#define FRAME_LEN_EXT 1023
+#ifdef ENABLE_PHR_EXT_MODE
+#define FRAME_LEN_MAX FRAME_LEN_EXT
+#else
+#define FRAME_LEN_MAX FRAME_LEN_STD
+#endif
 
+#define DEFAULT_RX_TIMEOUT 0xFFFFF
+
+/* Queue Constants */
+#define RX_QUEUE_SIZE 2
+#define RX_QUEUE_ITEM_SIZE sizeof(UWB_Packet_t)
+
+typedef uint16_t address_t;
+
+/* Packet */
+#define PACKET_SIZE FRAME_LEN_MAX
+#define PAYLOAD_SIZE (PACKET_SIZE - sizeof(Packet_Header_t))
+
+/* TX options */
 static dwt_txconfig_t txconfig_options = {
     .PGcount = 0x0,
     .PGdly = 0x34,
     .power = 0xfdfdfdfd
 };
-/* Default communication configuration. We use default non-STS DW mode. */
+
+/* PHR configuration */
 static dwt_config_t config = {
     5,            /* Channel number. */
     DWT_PLEN_128, /* Preamble length. Used in TX only. */
@@ -29,7 +50,11 @@ static dwt_config_t config = {
     1, /* 0 to use standard 8 symbol SFD, 1 to use non-standard 8 symbol, 2 for
           non-standard 16 symbol SFD and 3 for 4z 8 symbol SDF type */
     DWT_BR_6M8,      /* Data rate. */
-    DWT_PHRMODE_STD, /* PHY header mode. */
+#ifdef ENABLE_PHR_EXT_MODE
+    DWT_PHRMODE_EXT, /* Extended PHY header mode. */
+#else
+    DWT_PHRMODE_STD, /* Standard PHY header mode. */
+#endif
     DWT_PHRRATE_STD, /* PHY header rate. */
     (129 + 8 - 8), /* SFD timeout (preamble length + 1 + SFD length - PAC size).
                       Used in RX only. */
@@ -39,14 +64,28 @@ static dwt_config_t config = {
     DWT_PDOA_M0     /* PDOA mode off */
 };
 
-#define MY_UWB_ADDRESS 3
-#define TX_QUEUE_SIZE 5
-#define RX_QUEUE_SIZE 5
-#define TX_ITEM_SIZE sizeof(Ranging_Message_t)
-#define RX_ITEM_SIZE sizeof(Ranging_Message_With_Timestamp_t)
-#define RX_BUFFER_SIZE RX_ITEM_SIZE  // RX_BUFFER_SIZE ≤ FRAME_LEN_MAX
-#define Tf_BUFFER_POLL_SIZE 5
-#define TX_PERIOD_IN_MS 50
+/* UWB packet definition */
+typedef enum {
+  RANGING = 0,
+  FLOODING = 1,
+  DATA = 2,
+  MESSAGE_TYPE_COUNT, /* only used for counting message types. */
+} MESSAGE_TYPE;
+
+typedef struct {
+  mhr_802_15_4_t mac;    // mac header
+  struct {
+    MESSAGE_TYPE type: 6;
+    uint16_t length: 10;
+  };
+} __attribute__((packed)) Packet_Header_t;
+
+typedef struct {
+  Packet_Header_t header; // Packet header
+  uint8_t payload[PAYLOAD_SIZE]
+} __attribute__((packed)) UWB_Packet_t;
+
+typedef void (*UWBCallback)(void *);
 
 void uwbInit();
 void uwbStart();
--- src/uwb.c
+++ src/uwb.c
@@ -1,9 +1,9 @@
+#include <stm32f0xx_hal.h>
 #include "uwb.h"
 
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stm32f0xx_hal.h>
 #include <string.h>
 
 #include "FreeRTOS.h"
@@ -11,33 +11,28 @@
 #include "semphr.h"
 #include "task.h"
 #include "dwTypes.h"
+#include "libdw3000.h"
+#include "dw3000.h"
 
 extern dwOps_t dwt_ops;
 static bool isInit = false;
 static SemaphoreHandle_t irq_semphr;
 static SemaphoreHandle_t ranging_set_lock;
-static QueueHandle_t tx_queue;
-static StaticQueue_t tx_queue_buffer;
-static uint8_t tx_queue_storage[TX_QUEUE_SIZE * TX_ITEM_SIZE];
 
 static QueueHandle_t rx_queue;
 static StaticQueue_t rx_queue_buffer;
-static uint8_t rx_queue_storage[RX_QUEUE_SIZE * RX_ITEM_SIZE];
+static uint8_t rx_queue_storage[RX_QUEUE_SIZE * RX_QUEUE_ITEM_SIZE];
 
 /* rx buffer used in rx_callback */
-static uint8_t rx_buffer[RX_BUFFER_SIZE];
-Timestamp_Tuple_t Tf_buffer[Tf_BUFFER_POLL_SIZE] = {0};
-static int Tf_buffer_index = 0;
-static int seq_number = 0;
+static uint8_t rx_buffer[FRAME_LEN_MAX];
 
 void queueInit() {
-  tx_queue = xQueueCreateStatic(TX_QUEUE_SIZE, TX_ITEM_SIZE, tx_queue_storage,
-                                &tx_queue_buffer);
-  rx_queue = xQueueCreateStatic(RX_QUEUE_SIZE, RX_ITEM_SIZE, rx_queue_storage,
+  rx_queue = xQueueCreateStatic(RX_QUEUE_SIZE, RX_QUEUE_ITEM_SIZE, rx_queue_storage,
                                 &rx_queue_buffer);
 }
+
 SPI_HandleTypeDef hspi1;
-uint16_t alignedBuffer[64];
+uint8_t alignedBuffer[FRAME_LEN_MAX + 1];
 
 void spiRead(const void *header, size_t headerLength, void* data, size_t dataLength) {
   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, 0);
@@ -122,11 +117,8 @@ void dwOpsInit() {
 void uwbInit() {
   printf("uwbInit");
   queueInit();
-  ranging_table_set_init(&ranging_table_set);
   static StaticSemaphore_t irq_semphr_buffer;
   irq_semphr = xSemaphoreCreateBinaryStatic(&irq_semphr_buffer);
-  static StaticSemaphore_t ranging_set_lock_buffer;
-  ranging_set_lock = xSemaphoreCreateBinaryStatic(&ranging_set_lock_buffer);
   isInit = true;
   dwOpsInit();
   // port_set_dw_ic_spi_fastrate();
@@ -158,7 +150,7 @@ void uwbInit() {
   /* Auto re-enable receiver after a frame reception failure (except a frame
    * wait timeout), the receiver will re-enable to re-attempt reception.*/
   dwt_or32bitoffsetreg(SYS_CFG_ID, 0, SYS_CFG_RXAUTR_BIT_MASK);
-  dwt_setrxtimeout(UWB_RX_TIMEOUT);
+  dwt_setrxtimeout(0xfffff);
 
   dwt_setcallbacks(&tx_cb, &rx_cb, &rx_to_cb, &rx_err_cb, NULL, NULL);
   /* Enable wanted interrupts (TX confirmation, RX good frames, RX timeouts and
@@ -196,209 +188,29 @@ static void uwbTask(void* parameters) {
     }
   }
 }
-
-static void uwbTxTask(void* parameters) {
-  while (!isInit) {
-    printf("false");
-    vTaskDelay(1000);
-  }
-
-  Ranging_Message_t packetCache;
-
-  while (true) {
-    if (xQueueReceive(tx_queue, &packetCache, portMAX_DELAY)) {
-      // printf("===uwbTxTask===\r\n");
-      // print_ranging_message(&packetCache);
-      dwt_forcetrxoff();
-      dwt_writetxdata(packetCache.header.message_length, &packetCache, 0);
-      dwt_writetxfctrl(packetCache.header.message_length + FCS_LEN, 0, 1);
-      /* Start transmission. */
-      if (dwt_starttx(DWT_START_TX_IMMEDIATE | DWT_RESPONSE_EXPECTED) ==
-          DWT_ERROR) {
-        printf("uwbTxTask:  TX ERROR\r\n");
-      }
-    }
-  }
-}
-
-int16_t compute_distance(Ranging_Table_t* table) {
-
-  int64_t tRound1, tReply1, tRound2, tReply2, diff1, diff2, tprop_ctn;
-  tRound1 = (table->Rr.timestamp.full - table->Tp.timestamp.full + MAX_TIMESTAMP) % MAX_TIMESTAMP;
-  tReply1 = (table->Tr.timestamp.full - table->Rp.timestamp.full + MAX_TIMESTAMP) % MAX_TIMESTAMP;
-  tRound2 = (table->Rf.timestamp.full - table->Tr.timestamp.full + MAX_TIMESTAMP) % MAX_TIMESTAMP;
-  tReply2 = (table->Tf.timestamp.full - table->Rr.timestamp.full + MAX_TIMESTAMP) % MAX_TIMESTAMP;
-  diff1 = tRound1 - tReply1;
-  diff2 = tRound2 - tReply2;
-  tprop_ctn = (diff1 * tReply2 + diff2 * tReply1 + diff2 * diff1) / (tRound1 + tRound2 + tReply1 + tReply2);
-
-  int16_t distance = (int16_t) tprop_ctn * 0.4691763978616;
-  // printf("distance=%d cm\r\n", distance);
-  /* update ranging table */
-  table->Rp = table->Rf;
-  table->Tp = table->Tf;
-  table->Rr = table->Re;
-
-  table->Rf.timestamp.full = 0;
-  table->Rf.sequence_number = 0;
-  
-  table->Tf.timestamp.full = 0;
-  table->Tf.sequence_number = 0;
-
-  table->Tr.timestamp.full = 0;
-  table->Tr.sequence_number = 0;
-
-  return (int16_t)distance;
-}
-
-void process_ranging_message(
-  Ranging_Message_With_Timestamp_t* ranging_message_with_timestamp) {
-  Ranging_Message_t* ranging_message = &ranging_message_with_timestamp->ranging_message;
-  address_t neighbor_addr = ranging_message->header.source_address;
-  set_index_t neighbor_index = find_in_ranging_table_set(&ranging_table_set, neighbor_addr);
-  /* handle new neighbor */
-  if (neighbor_index == -1) {
-    if (ranging_table_set.free_queue_entry == -1) {
-      /* ranging table set is full */
-      return;
-    }
-    Ranging_Table_t table;
-    memset(&table, 0, sizeof(Ranging_Table_t));  // TODO check if necessary
-    table.neighbor_address = neighbor_addr;
-    table.period = TX_PERIOD_IN_MS;
-    table.next_delivery_time = xTaskGetTickCount() + table.period;
-    table.expiration_time = xTaskGetTickCount() + M2T(RANGING_TABLE_HOLD_TIME);
-    neighbor_index = ranging_table_set_insert(&ranging_table_set, &table);
-  }
-  Ranging_Table_t* neighbor_ranging_table = &ranging_table_set.set_data[neighbor_index].data;
-  /* update Re */
-  neighbor_ranging_table->Re.timestamp = ranging_message_with_timestamp->rx_time;
-  neighbor_ranging_table->Re.sequence_number = ranging_message->header.message_sequence;
-
-  Timestamp_Tuple_t neighbor_Tr = ranging_message->header.last_tx_timestamp;
-  // printf("##########neighbor Tr=%d#########\r\n", neighbor_Tr.sequence_number);
-  /* update Tr or Rr*/
-  if (neighbor_ranging_table->Tr.timestamp.full == 0) {
-    if (neighbor_ranging_table->Rr.sequence_number == neighbor_Tr.sequence_number) {
-      neighbor_ranging_table->Tr = neighbor_Tr;
-    } else {
-      neighbor_ranging_table->Rr = neighbor_ranging_table->Re;
-    }
-  }
-  Timestamp_Tuple_t neighbor_Rf = {.timestamp.full = 0};
-  uint8_t body_unit_count = (ranging_message->header.message_length - sizeof(Ranging_Message_Header_t)) / sizeof(Body_Unit_t);
-  for (int i = 0; i < body_unit_count; i++) {
-    if (ranging_message->body_units[i].address == MY_UWB_ADDRESS) {
-      neighbor_Rf = ranging_message->body_units[i].timestamp;
-      break;
-    }
-  }
-  /* update Rf and Tf*/
-  if (neighbor_Rf.timestamp.full) {
-    neighbor_ranging_table->Rf = neighbor_Rf;
-    /* find corresponding Tf in Tf_buffer */
-    for (int i = 0; i < Tf_BUFFER_POLL_SIZE; i++) {
-      if (Tf_buffer[i].sequence_number == neighbor_Rf.sequence_number) {
-        neighbor_ranging_table->Tf = Tf_buffer[i];
-      }
-    }
-  }
-
-  if (neighbor_ranging_table->Tr.timestamp.full && neighbor_ranging_table->Rf.timestamp.full && neighbor_ranging_table->Tf.timestamp.full) {
-      // printf("===before compute distance===\r\n");
-      // print_ranging_table(&ranging_table_set);
-      int16_t distance = compute_distance(neighbor_ranging_table);
-      printf("distance to neighbor %d = %d cm\r\n", ranging_message->header.source_address, distance);
-      // printf("===after compute distance===\r\n");
-      // print_ranging_table(&ranging_table_set);
-  } else if (neighbor_ranging_table->Rf.timestamp.full && neighbor_ranging_table->Tf.timestamp.full) {
-      neighbor_ranging_table->Rp = neighbor_ranging_table->Rf;
-      neighbor_ranging_table->Tp = neighbor_ranging_table->Tf;
-      neighbor_ranging_table->Rr = neighbor_ranging_table->Re;
-      neighbor_ranging_table->Rf.timestamp.full = 0;
-      neighbor_ranging_table->Tf.timestamp.full = 0;
-      neighbor_ranging_table->Tr.timestamp.full = 0;
-  }
-  /* update expiration time */
-  neighbor_ranging_table->expiration_time = xTaskGetTickCount() + M2T(RANGING_TABLE_HOLD_TIME);
-}
-
+ 
 static void uwbRxTask(void* parameters) {
   while (!isInit) {
     printf("false");
     vTaskDelay(1000);
   }
 
-  Ranging_Message_With_Timestamp_t rx_packet_cache;
+  UWB_Packet_t rx_packet_cache;
 
   while (true) {
     if (xQueueReceive(rx_queue, &rx_packet_cache, portMAX_DELAY)) {
-      // xSemaphoreTake(ranging_set_lock, portMAX_DELAY);
-      // printf("before process ranging table\r\n");
-      // print_ranging_table(&ranging_table_set);
-      // printf("after process ranging table\r\n");
-      // printf("===uwbRxTask===\r\n");
-      // print_ranging_message(&rx_packet_cache.ranging_message);
-      process_ranging_message(&rx_packet_cache);
-      // print_ranging_table(&ranging_table_set);
-      // xSemaphoreGive(ranging_set_lock);
+      uint16_t * payload = &rx_packet_cache.payload;
+      if (rx_packet_cache.header.type == RANGING) {
+        printf("Received Ranging Message \r\n");
+        printf("Received from %d, length = %d \r\n", payload[0], rx_packet_cache.header.length);
+      }
+      if (rx_packet_cache.header.type == FLOODING) {
+        printf("Received Flooding Message \r\n");
+      }
+      if (rx_packet_cache.header.type == DATA) {
+        printf("Received Data Message \r\n");
+      }
     }
-  }
-}
-
-static int get_sequence_number() {
-  seq_number++;
-  return seq_number;
-}
-
-static void generate_ranging_message(Ranging_Message_t* ranging_message) {
-  /* generate body unit */
-  int8_t body_unit_number = 0;
-  int cur_seq_number = get_sequence_number();
-
-  for (set_index_t index = ranging_table_set.full_queue_entry; index != -1;
-       index = ranging_table_set.set_data[index].next) {
-    Ranging_Table_t* table = &ranging_table_set.set_data[index].data;
-    if (body_unit_number >= MAX_BODY_UNIT_NUMBER) {
-      break;
-    }
-    if (table->Re.timestamp.full) {
-      ranging_message->body_units[body_unit_number].address =
-          table->neighbor_address;
-      ranging_message->body_units[body_unit_number].timestamp = table->Re;
-      body_unit_number++;
-      table->Re.sequence_number = 0;
-      table->Re.timestamp.full = 0;
-    }
-  }
-  /* generate message header */
-  ranging_message->header.source_address = MY_UWB_ADDRESS;
-  ranging_message->header.message_length = sizeof(Ranging_Message_Header_t) + sizeof(Body_Unit_t) * body_unit_number;
-  // printf("Tx Task Generate: size of ranging_message=%d\r\n", ranging_message->header.message_length);
-  // printf("Tx Task Generate: number of body_unit=%d\r\n", body_unit_number);
-//   printf("size of message_header=%d, size of body_unit=%d \r\n", sizeof(Ranging_Message_Header_t), sizeof(Body_Unit_t));
-  ranging_message->header.message_sequence = cur_seq_number;
-  ranging_message->header.last_tx_timestamp = Tf_buffer[Tf_buffer_index];
-  ranging_message->header.velocity = 0;
-}
-
-static void uwbRangingTask(void* parameters) {
-  while (!isInit) {
-    printf("false");
-    vTaskDelay(1000);
-  }
-
-  while (true) {
-    // xSemaphoreTake(ranging_set_lock, portMAX_DELAY);
-
-    Ranging_Message_t tx_packet_cache;
-    generate_ranging_message(&tx_packet_cache);
-    // printf("===Tx Task===\r\n");
-    // print_ranging_message(&tx_packet_cache);
-    xQueueSend(tx_queue, &tx_packet_cache, portMAX_DELAY);
-    // xSemaphoreGive(ranging_set_lock);
-
-    vTaskDelay(TX_PERIOD_IN_MS);
   }
 }
 
@@ -410,26 +222,12 @@ void uwbStart() {
   xTaskCreateStatic(uwbTask, "uwbTask", configMINIMAL_STACK_SIZE, NULL,
                     configMAX_PRIORITIES - 1, uwbStaticStack, &uwbStaticTask);
 
-  static StaticTask_t uwbTxStaticTask;
-  static StackType_t uwbTxStaticStack[2 * configMINIMAL_STACK_SIZE];
-
-  xTaskCreateStatic(uwbTxTask, "uwbTxTask", 2 * configMINIMAL_STACK_SIZE, NULL,
-                    configMAX_PRIORITIES - 1, uwbTxStaticStack,
-                    &uwbTxStaticTask);
-
   static StaticTask_t uwbRxStaticTask;
   static StackType_t uwbRxStaticStack[2 * configMINIMAL_STACK_SIZE];
 
   xTaskCreateStatic(uwbRxTask, "uwbRxTask", 2 * configMINIMAL_STACK_SIZE, NULL,
                     configMAX_PRIORITIES - 1, uwbRxStaticStack,
                     &uwbRxStaticTask);
-
-  static StaticTask_t uwbRangingStaticTask;
-  static StackType_t uwbRangingStaticStack[2 * configMINIMAL_STACK_SIZE];
-
-  xTaskCreateStatic(
-      uwbRangingTask, "uwbRangingTask", 2 * configMINIMAL_STACK_SIZE, NULL,
-      configMAX_PRIORITIES - 1, uwbRangingStaticStack, &uwbRangingStaticTask);
 }
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
@@ -454,35 +252,26 @@ void rx_cb() {
     dwt_readrxdata(rx_buffer, data_length - FCS_LEN,
                    0); /* No need to read the FCS/CRC. */
   }
-  // printf("===Rx callback===\r\n");
-  // printf("rx_data length = %u \r\n", data_length - FCS_LEN);
-  // print_ranging_message(&rx_buffer);
-  dw_time_t rx_time;
-  dwt_readrxtimestamp(&rx_time.raw);
-  Ranging_Message_With_Timestamp_t rx_message_with_timestamp;
-  rx_message_with_timestamp.rx_time = rx_time;
-  Ranging_Message_t* ranging_message = &rx_buffer;
-  rx_message_with_timestamp.ranging_message = *ranging_message;
-  xQueueSendFromISR(rx_queue, &rx_message_with_timestamp,
+  UWB_Packet_t *packet = (UWB_Packet_t *) &rx_buffer;
+  printf("===Rx callback===\r\n");
+  printf("rx_data length = %d \r\n", data_length - FCS_LEN);
+
+  xQueueSendFromISR(rx_queue, packet,
                     &xHigherPriorityTaskWoken);
   dwt_forcetrxoff();
   dwt_rxenable(DWT_START_RX_IMMEDIATE);
 }
 
 void tx_cb() {
-  dw_time_t tx_time;
-  dwt_readtxtimestamp(&tx_time.raw);
-  // printf("===Tx callback===\r\n");
-  // printf("Current Tx Timestasmp: %2x%8lx \r\n",tx_time.high8,tx_time.low32);
-  Tf_buffer_index++;
-  Tf_buffer_index %= Tf_BUFFER_POLL_SIZE;
-  Tf_buffer[Tf_buffer_index].sequence_number = seq_number;
-  Tf_buffer[Tf_buffer_index].timestamp = tx_time;
+    printf("tx_cb\r\n");
 }
 
 void rx_to_cb() {
   dwt_forcetrxoff();
   dwt_rxenable(DWT_START_RX_IMMEDIATE);
+  printf("rx_timeout_cb\r\n");
 }
 
-void rx_err_cb() { printf("rx_err_cb\r\n"); }
+void rx_err_cb() {
+  printf("rx_err_cb\r\n"); 
+}
